a9522f5120fbc390256c95d97b2afbf50ad3b7f7
mocch62 <159973236+mocch62@users.noreply.github.com>
2024-04-18 09:05:24 +0900

    Add files via upload
    再提出

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -1,8 +1,8 @@
 const date = new Date();
 const [month, day, year] = [
-  date.getMonth(),
+  date.getMonth() + 1,
   date.getDate(),
   date.getFullYear(),
 ];
 
-console.log(date);
+console.log(year + "年" + month + "月" + day + "日");
